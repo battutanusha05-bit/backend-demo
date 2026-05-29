2bd05ec41e98b3103f8773b72324e0e68c4fad55
EC2 Default User <ec2-user@ip-172-31-3-197.ec2.internal>
2026-05-29 12:00:54 +0000

    updated code

--- src/main/java/com/example/kapidemo/KapidemoApplication.java
+++ src/main/java/com/example/kapidemo/KapidemoApplication.java
@@ -2,12 +2,14 @@ package com.example.kapidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KapidemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KapidemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KapidemoApplication.class, args);
+    }
 
 }
+
